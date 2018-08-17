1a2bf068ab70e9d9446b50473bfa7199d114ec22
Arthur Chang <arthurc0102@gmail.com>
2018-08-17 15:47:34 +0800

    Add flag config support

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # key-generator
 
 > Random key generator
+
+## Install
+
+- Run: `go get -u github.com/arthurc0102/key-generator`
+
+## Use
+
+- See help: `key-generator -h`
--- main.go
+++ main.go
@@ -1,38 +1,82 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math/rand"
 	"os"
-	"strconv"
 	"time"
 )
 
-const target = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#%^&*(-_=+)"
+const (
+	lowercase   = "abcdefghijklmnopqrstuvwxyz"
+	uppercase   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digits      = "0123456789"
+	punctuation = "!#$%&()*+,-./:;<=>?@[]^_`{|}~"
+)
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
 func main() {
-	keyLen := 50
+	var (
+		l, u, d, p bool
+		repeat     bool
+		length     int
+		target     string
+	)
 
-	if len(os.Args) > 1 {
-		args := os.Args
+	flag.BoolVar(&l, "l", true, "Use lowercase char in target")
+	flag.BoolVar(&u, "u", true, "Use uppercase char in target")
+	flag.BoolVar(&d, "d", true, "Use digits char in target")
+	flag.BoolVar(&p, "p", true, "Use punctuation in target")
+	flag.BoolVar(&repeat, "r", true, "Char can be used for many times")
+	flag.IntVar(&length, "len", 50, "Length for key")
+	flag.Parse()
 
-		if k, err := strconv.Atoi(args[1]); err != nil {
-			fmt.Printf("Key len should be int, got '%s'\n", args[1])
-			os.Exit(1)
-		} else {
-			keyLen = k
+	if length < 1 {
+		log.Fatalln("Length should be bigger than 0")
+	}
+
+	config := []bool{l, u, d, p}
+	chars := []string{lowercase, uppercase, digits, punctuation}
+
+	if len(config) != len(chars) {
+		panic("Len of 'config' and 'chars' should be the same")
+	}
+
+	for i := range config {
+		if !config[i] {
+			continue
+		}
+
+		target += chars[i]
+	}
+
+	targetLen := len(target)
+
+	if !repeat && int(length) > targetLen {
+		fmt.Println("Your key length is smaller then target, you can:")
+		fmt.Println("  - Make your key length shorter")
+		fmt.Println("  - Make your target longer")
+		fmt.Println("  - Allow repeat")
+		os.Exit(1)
+	}
+
+	result := make([]byte, length)
+
+	if repeat {
+		for i := range result {
+			result[i] = target[rand.Intn(targetLen)]
+		}
+	} else {
+		shuffle := rand.Perm(targetLen)
+		for i := range result {
+			result[i] = target[shuffle[i]]
 		}
 	}
 
-	key := make([]byte, keyLen)
-
-	for i := range key {
-		key[i] = target[rand.Intn(len(target))]
-	}
-
-	fmt.Println(string(key))
+	fmt.Println(string(result))
 }
